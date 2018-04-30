dcf3345a031b14476e1dbac03031db24890363e8
Nathan <nathan.apb63@gmail.com>
2018-04-30 21:07:14 +0200

    main

--- main.cpp
+++ main.cpp
@@ -6,5 +6,13 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 	cellule c('m');// test cellule
+	//test cellule::is_dead et cellule::is_mutating
+	int morts =0;
+	int mutations=0;
+	for (int i(0);i<=4;i++){
+		if(c.is_dead(0.5)){morts++;}
+		if(c.is_mutating(0.5)){mutations++;}
+	}
+	cout << morts << "morts et "<<mutations<< "mutations.\n"<<endl; //resultat bizarre je tombe sur 7 7 beaucoup trop souvent à voir..
 	return 0;
 }
