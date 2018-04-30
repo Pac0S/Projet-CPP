25a40823ce3b306b77980160f3575f37bd55aa1f
Nathan <nathan.apb63@gmail.com>
2018-04-30 21:21:41 +0200

    function test booleans

--- main.cpp
+++ main.cpp
@@ -8,12 +8,20 @@ using namespace std;
 int main(int argc, char* argv[]){
 	cellule c('m');// test cellule
 	//test cellule::is_dead et cellule::is_mutating
-	int morts =0;
-	int mutations=0;
-	for (int i(0);i<=4;i++){
-		if(c.is_dead(0.5)){morts++;}
-		if(c.is_mutating(0.5)){mutations++;}
-	}
-	cout << morts << "morts et "<<mutations<< "mutations.\n"<<endl; //resultat bizarre je tombe sur 7 7 beaucoup trop souvent à voir..
+	
+	
 	return 0;
 }
+
+void test_bool_function(cellule c,int try_nbr, int Pdeath, int Pmutation){
+	int morts =0;
+	int mutations=0;
+	for (int i(0);i<=try_nbr;i++){
+		if(c.is_dead(Pdeath)){morts++;}
+		if(c.is_mutating(Pmutation)){mutations++;}
+	}
+	cout << morts << "morts et "<<mutations<< "mutations.\n"<<endl; //resultat bizarre je tombe sur 7 7 beaucoup trop souvent à voir..
+	
+	
+}
+	
